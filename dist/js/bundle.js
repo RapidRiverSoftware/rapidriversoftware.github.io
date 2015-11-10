fcfa94f4e3900ecefbba3833c595c32dc152302a
Mutahhir <mutahhir.ali@gmail.com>
2015-11-10 07:43:48 +0500

    Update build

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -8323,20 +8323,171 @@ $__System.registerDynamic("5", ["4"], true, function(req, exports, module) {
   return module.exports;
 });
 
-$__System.register('1', ['3', '5'], function (_export) {
+$__System.register('6', [], function (_export) {
+	/*!
+  * avgrund 0.1
+  * http://lab.hakim.se/avgrund
+  * MIT licensed
+  *
+  * Copyright (C) 2012 Hakim El Hattab, http://hakim.se
+  *
+  *
+  * Modifications by Mutahhir Ali Hayat <mutahhir.ali@gmail.com>
+  * - make it into an ES6 module
+  * - allow custom height popups
+  */
+	'use strict';
+
+	return {
+		setters: [],
+		execute: function () {
+			_export('default', (function () {
+
+				var container = document.documentElement,
+				    popup = document.querySelector('.avgrund-popup-animate'),
+				    cover = document.querySelector('.avgrund-cover'),
+				    currentState = null;
+
+				container.className = container.className.replace(/\s+$/gi, '') + ' avgrund-ready';
+
+				// Deactivate on ESC
+				function onDocumentKeyUp(event) {
+					if (event.keyCode === 27) {
+						deactivate();
+					}
+				}
+
+				// Deactivate on click outside
+				function onDocumentClick(event) {
+					if (event.target === cover) {
+						deactivate();
+					}
+				}
+
+				function setupPosition() {
+					var windowWidth = $(window).width();
+					var windowHeight = $(window).height();
+					var popupWidth = $(popup).width();
+					windowWidth = windowWidth * 0.9; // scaling factor used in animation
+
+					var popupRect = popup.getBoundingClientRect();
+					var left = Math.abs(windowWidth - popupWidth) / 2;
+					var top = (windowHeight - popupRect.height) / 2;
+					var leftPerc = left / windowWidth * 100;
+
+					if (leftPerc < 0) leftPerc = 0;
+
+					$(popup).css('left', leftPerc + '%');
+				}
+
+				function onResize() {
+					setTimeout(setupPosition, 0);
+				}
+
+				function activate(state) {
+					document.addEventListener('keyup', onDocumentKeyUp, false);
+					document.addEventListener('click', onDocumentClick, false);
+					document.addEventListener('touchstart', onDocumentClick, false);
+					window.addEventListener('resize', onResize, false);
+
+					removeClass(popup, currentState);
+					addClass(popup, 'no-transition');
+					addClass(popup, state);
+
+					setupPosition();
+
+					setTimeout(function () {
+						removeClass(popup, 'no-transition');
+						addClass(container, 'avgrund-active');
+					}, 0);
+
+					currentState = state;
+				}
+
+				function deactivate() {
+					document.removeEventListener('keyup', onDocumentKeyUp, false);
+					document.removeEventListener('click', onDocumentClick, false);
+					document.removeEventListener('touchstart', onDocumentClick, false);
+					window.removeEventListener('resize', onResize, false);
+
+					removeClass(container, 'avgrund-active');
+					removeClass(popup, 'avgrund-popup-animate');
+				}
+
+				function disableBlur() {
+					addClass(document.documentElement, 'no-blur');
+				}
+
+				function addClass(element, name) {
+					element.className = element.className.replace(/\s+$/gi, '') + ' ' + name;
+				}
+
+				function removeClass(element, name) {
+					element.className = element.className.replace(name, '');
+				}
+
+				function show(selector) {
+					popup = document.querySelector(selector);
+					addClass(popup, 'avgrund-popup-animate');
+					activate();
+					return this;
+				}
+				function hide() {
+					deactivate();
+				}
+
+				return {
+					activate: activate,
+					deactivate: deactivate,
+					disableBlur: disableBlur,
+					show: show,
+					hide: hide
+				};
+			})());
+		}
+	};
+});
+$__System.register('1', ['3', '5', '6'], function (_export) {
   'use strict';
 
-  var $, BS;
+  var $, BS, Avgrund;
+
+  function handleTeamPopups() {
+    $('.team__member').on('click', function (e) {
+      var $this = $(this);
+      console.log(this);
+      var name = $this.data('detail');
+      var templatePath = '/templates/team/' + name + '.template.html';
+
+      $.ajax({
+        url: templatePath,
+        context: document.body,
+        dataType: 'html'
+      }).success(function (data) {
+        $('.dialog').remove();
+
+        $(document.body).prepend('<div class="dialog avgrund-popup">' + data + '</div>');
+
+        Avgrund.show(".dialog");
+      });
+    });
+
+    $(document).on('click', '.dialog-closer', function (e) {
+      Avgrund.hide();
+    });
+  }
+
   return {
     setters: [function (_) {
       $ = _['default'];
     }, function (_2) {
       BS = _2['default'];
+    }, function (_3) {
+      Avgrund = _3['default'];
     }],
     execute: function () {
-
       $(function () {
-        console.log("Hello world!");
+        handleTeamPopups();
       });
     }
   };
