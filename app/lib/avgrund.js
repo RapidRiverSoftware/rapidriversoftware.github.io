8bb69a0ed1214d630b75b2d00edc5a2b3c427d70
Mutahhir <mutahhir.ali@gmail.com>
2015-11-10 06:28:19 +0500

    Start work on team popups

---
+++ app/lib/avgrund.js
@@ -0,0 +1,103 @@
+/*!
+ * avgrund 0.1
+ * http://lab.hakim.se/avgrund
+ * MIT licensed
+ *
+ * Copyright (C) 2012 Hakim El Hattab, http://hakim.se
+ *
+ *
+ * Modifications by Mutahhir Ali Hayat <mutahhir.ali@gmail.com> to make it into an ES6 module
+ */
+export default (function () {
+
+	var container = document.documentElement,
+		popup = document.querySelector( '.avgrund-popup-animate' ),
+		cover = document.querySelector( '.avgrund-cover' ),
+		currentState = null;
+
+	container.className = container.className.replace( /\s+$/gi, '' ) + ' avgrund-ready';
+
+	// Deactivate on ESC
+	function onDocumentKeyUp( event ) {
+		if( event.keyCode === 27 ) {
+			deactivate();
+		}
+	}
+
+	// Deactivate on click outside
+	function onDocumentClick( event ) {
+		if( event.target === cover ) {
+			deactivate();
+		}
+	}
+
+  function setupPosition () {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+
+    var popupRect = popup.getBoundingClientRect();
+    var left = (windowWidth - popupRect.width) / 2;
+    var top = (windowHeight - popupRect.height) / 2;
+
+    $(popup).css('left', left);
+  }
+
+	function activate( state ) {
+		document.addEventListener( 'keyup', onDocumentKeyUp, false );
+		document.addEventListener( 'click', onDocumentClick, false );
+		document.addEventListener( 'touchstart', onDocumentClick, false );
+
+		removeClass( popup, currentState );
+		addClass( popup, 'no-transition' );
+		addClass( popup, state );
+
+    setupPosition();
+
+		setTimeout( function() {
+			removeClass( popup, 'no-transition' );
+			addClass( container, 'avgrund-active' );
+		}, 0 );
+
+		currentState = state;
+	}
+
+	function deactivate() {
+		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
+		document.removeEventListener( 'click', onDocumentClick, false );
+		document.removeEventListener( 'touchstart', onDocumentClick, false );
+
+		removeClass( container, 'avgrund-active' );
+		removeClass( popup, 'avgrund-popup-animate')
+	}
+
+	function disableBlur() {
+		addClass( document.documentElement, 'no-blur' );
+	}
+
+	function addClass( element, name ) {
+		element.className = element.className.replace( /\s+$/gi, '' ) + ' ' + name;
+	}
+
+	function removeClass( element, name ) {
+		element.className = element.className.replace( name, '' );
+	}
+
+	function show(selector){
+		popup = document.querySelector( selector );
+		addClass(popup, 'avgrund-popup-animate');
+		activate();
+		return this;
+	}
+	function hide() {
+		deactivate();
+	}
+
+	return {
+		activate: activate,
+		deactivate: deactivate,
+		disableBlur: disableBlur,
+		show: show,
+		hide: hide
+	}
+
+})();
--- app/lib/main.js
+++ app/lib/main.js
@@ -1,7 +1,30 @@
 import $ from 'jquery';
-
 import BS from 'bootstrap';
+import Avgrund from './avgrund';
+
+function handleTeamPopups() {
+  $('.team__member').on('click', function (e) {
+    let $this = $(this);
+    console.log(this);
+    const name = $this.data('detail');
+    const templatePath = `/templates/team/${name}.template.html`;
+
+    $.ajax({
+      url: templatePath,
+      context: document.body,
+      dataType: 'html'
+    }).success((data) => {
+      $('.dialog').remove();
+
+      $(document.body)
+        .prepend(`<div class="dialog avgrund-popup">${data}</div>`);
+
+      Avgrund.show(".dialog");
+    });
+
+  })
+}
 
 $(() => {
-  console.log("Hello world!");
-})
+  handleTeamPopups();
+});
