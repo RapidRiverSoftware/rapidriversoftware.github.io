492101b79fdf55439bb24b67cb5e1b06c96d8e57
Snowflakekiller <karuna.skj@gmail.com>
2016-08-15 22:24:20 +0800

    update email address

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/karuna@rrsoft.co",
+                url: "https://formspree.io/info@rrsoft.co",
                 type: "POST",
                 data: {
                     name: name,
