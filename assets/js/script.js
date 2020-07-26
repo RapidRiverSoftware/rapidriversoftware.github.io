1e4a07071c044186d30a4e2bdeeb1fb74e89ab32
Shaii Ong <monsteronfire@gmail.com>
2020-07-27 04:40:44 +0800

    Clean up theme JS and rename file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-/*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-    window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
-
 // Highlight active navigation menu item
 $(function() {
   var fullPath = window.location.pathname.substring(1);
