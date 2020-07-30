d9ff34f1d5fefd97b35ebc7fc1e87bf79ec99115
Shaii Ong <monsteronfire@gmail.com>
2020-07-30 13:28:51 +0800

    Clean up Services SVG

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -6,13 +6,16 @@
   // Store Elements
   var servicesSection = document.querySelector('[data-fx="services-section"]');
   var teamSection = document.querySelector('[data-fx="team-section"]');
-  var myElement = document.querySelector('[data-fx="flying-postit"]');
+  var flyingPostit = document.querySelector('[data-fx="flying-postit"]');
 
   var speechBubble1 = document.querySelector('[data-fx="speech-bubble-1"]');
   var speechBubble2 = document.querySelector('[data-fx="speech-bubble-2"]');
   var speechBubble3 = document.querySelector('[data-fx="speech-bubble-3"]');
   var speechBubble4 = document.querySelector('[data-fx="speech-bubble-4"]');
   var speechBubble5 = document.querySelector('[data-fx="speech-bubble-5"]');
+
+  var timelineServices = gsap.timeline({repeat: 2, repeatDelay: 1});
+  var timelineTeam = gsap.timeline({repeat: 2, repeatDelay: 1});
 
   function debounce(func, wait, immediate) {
     var timeout;
@@ -43,24 +46,23 @@
 
   var postitFx = debounce(function() {
     console.log(isElementInView(servicesSection));
-    gsap.to(myElement, {
-      duration: 5,
-      repeat: 5,
-      repeatDelay: 5,
-      yoyo: true,
-      ease: "power1.inOut",
-      motionPath: {
-        path: "[data-fx='postit-path']",
-        align: "[data-fx='postit-path']",
-        autoRotate: true,
-        alignOrigin: [0.5, 0.5]
-      }
-    });
+    // gsap.to(flyingPostit, {
+    //   duration: 5,
+    //   repeat: 5,
+    //   repeatDelay: 5,
+    //   yoyo: true,
+    //   ease: "power1.inOut",
+    //   motionPath: {
+    //     path: "[data-fx='postit-path']",
+    //     align: "[data-fx='postit-path']",
+    //     autoRotate: true,
+    //     alignOrigin: [0.5, 0.5]
+    //   }
+    // });
   }, 15);
 
   var teamFx = debounce(function() {
     console.log(isElementInView(teamSection));
-    var timelineTeam = gsap.timeline({repeat: 2, repeatDelay: 1});
     timelineTeam.from(speechBubble1, {x: 80, duration: 1, opacity:0});
     timelineTeam.from(speechBubble2, {x: -80, duration: 1, opacity:0});
     timelineTeam.from(speechBubble3, {x: 80, duration: 1, opacity:0});
