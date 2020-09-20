555371d5422e13b1acfcadad9c83332557c53ed8
Shaii Ong <monsteronfire@gmail.com>
2020-09-21 02:12:06 +0800

    Implement background display fn on profile expand

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,8 +1,17 @@
 (function() {
   var RESET_INTERVAL = 10000;
+  var portrait = 'portrait';
+  var background = 'background';
   var timedRemoval;
   var teamProfiles = document.querySelectorAll('[data-fx="profile"]');
   var allSocialLinks = document.querySelectorAll('[data-fx="social-link"]');
+
+  // TODO List
+  // 1. Switch profile photo
+  // 2. Add portrait/background toggle feature
+  // 3. Edit all images down to the appropriate size
+  // 4. Do something about mobile. Reduce functionality so as not
+  //    to overload it.
 
   var toggleClass = function(e, c) {
     (e).classList.toggle(c)
@@ -35,16 +44,17 @@
     });
   };
 
-  var setBackgroundPortrait = function(targetProfile) {
-    targetProfile.style.backgroundColor = 'red';
+  var setBackgroundPortrait = function(targetProfile, imgType) {
+    var slug = targetProfile.getAttribute('data-slug')
+    targetProfile.style.backgroundImage = 'url("/assets/img/team/' + imgType + '/' + slug +'.jpg")';
   };
 
   var expandProfile = function() {
     teamProfiles.forEach(function(profile, index) {
       profile.addEventListener('click', function(event) {
         // reset();
+        setBackgroundPortrait(profile, portrait);
         toggleClass(this, 'fx-expand');
-        setBackgroundPortrait(profile);
         // setTimer();
       })
     });
