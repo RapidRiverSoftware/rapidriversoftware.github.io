78cce6cb9e12ab88f94e0906ba80fa38ed19edbb
Shaii Ong <monsteronfire@gmail.com>
2020-09-21 01:32:59 +0800

    Make sure social links don't trigger expand fn

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,13 +1,15 @@
 (function() {
+  var RESET_INTERVAL = 10000;
   var timedRemoval;
-  var teamProfiles = document.querySelectorAll('.profile');
+  var teamProfiles = document.querySelectorAll('[data-fx="profile"]');
+  var allSocialLinks = document.querySelectorAll('[data-fx="social-link"]');
 
   var toggleClass = function(e, c) {
     (e).classList.toggle(c)
   };
 
   var setTimer = function() {
-    timedRemoval = setTimeout(removeExpand, 8000);
+    timedRemoval = setTimeout(removeExpand, RESET_INTERVAL);
   };
 
   var clearTimer = function() {
@@ -25,12 +27,33 @@
     removeExpand();
   };
 
-  // TODO: put this in an init function once you're a little more happy with it
-  teamProfiles.forEach(function(profile, index) {
-    profile.addEventListener('click', function(event) {
-      reset();
-      toggleClass(this, 'fx-expand');
-      setTimer();
-    })
-  });
+  var preventLinkBubble = function() {
+    allSocialLinks.forEach(function(socialLink) {
+      socialLink.addEventListener('click', function(event) {
+        event.stopPropagation();
+      });
+    });
+  };
+
+  var setBackgroundPortrait = function(targetProfile) {
+    targetProfile.style.backgroundColor = 'red';
+  };
+
+  var expandProfile = function() {
+    teamProfiles.forEach(function(profile, index) {
+      profile.addEventListener('click', function(event) {
+        // reset();
+        toggleClass(this, 'fx-expand');
+        setBackgroundPortrait(profile);
+        // setTimer();
+      })
+    });
+  };
+
+  var init = function() {
+    preventLinkBubble();
+    expandProfile();
+  };
+
+  init();
 })();
