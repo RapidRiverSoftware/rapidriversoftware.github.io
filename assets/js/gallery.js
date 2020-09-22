ac859fe48d0a8c4c669cc62d28cb6759a8a22f5c
Shaii Ong <monsteronfire@gmail.com>
2020-09-22 21:38:18 +0800

    Toggle thumbnail as well

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,15 +1,18 @@
 (function() {
-  var RESET_INTERVAL = 10000;
+  var RESET_INTERVAL = 7000;
+  var IMG_DIR = '/assets/img/team/';
+  var IMG_FORMAT = '.jpg';
   var portrait = 'portrait';
   var background = 'background';
+  var thumbnail = 'thumbnail';
+  var thumbBg = 'thumbnail-background';
   var timedRemoval;
   var allToggleButtons = document.querySelectorAll('[data-fx="toggle-button"] input');
   var teamProfiles = document.querySelectorAll('[data-fx="profile"]');
   var allNestedLinks = document.querySelectorAll('[data-info="nested-link"]');
 
   // TODO List
-  // 1. Switch profile photo when toggle button is :checked
-  // 2. Do something about mobile. Reduce functionality so as not
+  // 1. Do something about mobile. Reduce functionality so as not
   //    to overload it.
 
   var toggleClass = function(e, c) {
@@ -24,15 +27,29 @@
     clearTimeout(timedRemoval);
   };
 
-  var removeExpand = function() {
-    teamProfiles.forEach(function(profile) {
-      profile.classList.remove('fx-expand');
-    });
-  };
-
   var reset = function() {
     clearTimer();
     removeExpand();
+  };
+
+  var setBackground = function(targetProfile, imgType) {
+    var slug = targetProfile.getAttribute('data-slug');
+    var imgPath = `${IMG_DIR}${imgType}/${slug}${IMG_FORMAT}`;
+    targetProfile.style.backgroundImage = `url(${imgPath})`;
+  };
+
+  var setThumbnailSrc = function(targetProfile, thumbnailType) {
+    var slug = targetProfile.getAttribute('data-slug');
+    var thumbnail = targetProfile.querySelector('[data-fx="thumbnail-photo"]');
+    var srcPath = `${IMG_DIR}${thumbnailType}/${slug}${IMG_FORMAT}`;
+    thumbnail.src = srcPath;
+  };
+
+  var removeExpand = function() {
+    teamProfiles.forEach(function(profile) {
+      profile.classList.remove('fx-expand');
+      setThumbnailSrc(profile, thumbnail);
+    });
   };
 
   var preventLinkBubble = function() {
@@ -43,19 +60,16 @@
     });
   };
 
-  var setBackground = function(targetProfile, imgType) {
-    var slug = targetProfile.getAttribute('data-slug')
-    targetProfile.style.backgroundImage = 'url("/assets/img/team/' + imgType + '/' + slug +'.jpg")';
-  };
-
   var toggleMemberBackground = function() {
     allToggleButtons.forEach(function(tgBtn) {
       tgBtn.addEventListener('change', function(event) {
         var parentProfile = this.closest('[data-fx="profile"]')
         if (this.checked) {
           setBackground(parentProfile, background);
+          setThumbnailSrc(parentProfile, thumbnail);
         } else {
           setBackground(parentProfile, portrait);
+          setThumbnailSrc(parentProfile, thumbBg);
         }
       });
     });
@@ -64,11 +78,11 @@
   var expandProfile = function() {
     teamProfiles.forEach(function(profile, index) {
       profile.addEventListener('click', function(event) {
-        // reset();
+        reset();
         setBackground(profile, portrait);
+        setThumbnailSrc(profile, thumbBg);
         toggleClass(this, 'fx-expand');
-        toggleMemberBackground();
-        // setTimer();
+        setTimer();
       })
     });
   };
@@ -76,6 +90,7 @@
   var init = function() {
     preventLinkBubble();
     expandProfile();
+    toggleMemberBackground();
   };
 
   init();
